4a3cb0fb608c0fea4601f44bcd2a83f03bd39a08
pan93412 <pan93412@gmail.com>
2022-09-06 12:57:15 +0800

    Commit reproducible code

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+struct TestStruct {
+    position_currency: String, // Currency for position of this contract. If not null, 1 contract = 1 positionCurrency.
+    pu: Option<i64>, // Previous event update sequense ("u" of previous message), -1 also means None
+}
